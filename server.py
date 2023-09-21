6dd70798dd2d20646237151c8b6604932cd83f9b
tincho <martinmartossimon@gmail.com>
2023-09-21 03:39:49 -0300

    pmv - Envio del video a los clientes web

--- server.py
+++ server.py
@@ -1,78 +1,45 @@
 #!/usr/bin/python3
-
 import http.server
+from http.server import BaseHTTPRequestHandler
+from http.server import ThreadingHTTPServer
 import socketserver
+import socket
 import json
 import os
 import threading
 import subprocess
 import urllib.parse
 import time
+import datetime
+
 
 directorio = "./contenido"
 mensajes = []
 clientes = []
+clientes_eventos = []
+
 
 def listar_archivos():
-    #print("Entro a listar_archivos()")
     try:
-        #archivos = os.listdir(directorio)
-        #archivos = [nombre for nombre in os.listdir(directorio) if not nombre.startswith('.')]
         archivos = [nombre for nombre in os.listdir(directorio) if nombre.endswith('.mp4')]
         # Ordena la lista de archivos por fecha de creación ascendente
-        # Ordena la lista de archivos por fecha de creación ascendente
         archivos_ordenados = sorted(archivos, key=lambda x: os.path.getctime(os.path.join(directorio, x)))
-
-        # Invierte la lista para tener el orden más reciente primero
-        #archivos_ordenados = archivos_ordenados[::-1]
-
-        #print("Listado de archivos leidos: " + str(archivos))
-        #print("Listado de archivos leidos ordenados: " + str(archivos_ordenados))
-
-        #return archivos
         return archivos_ordenados
     except Exception as e:
         return str(e)
 
-
 def descargarVideoYoutube(url):
-    print("Entrando al método descargarVideoYoutube con url: '" + url + "'")
-    #urlLimpia = urllib.parse.quote(url)
     urlLimpia = urllib.parse.unquote(url)
-    #script_path = os.path.abspath('./descargadorYtb-dlp')
     script_path = os.path.abspath('/home/tincho/Scripts/reproductor_remoto/descargadorYtb-dlp')
-    #script_path = os.path.abspath('/home/tincho/Scripts/reproductor_remoto/testScript.sh')
-
     try:
-        #Funciona
-        #subprocess.run(["sh", script_path, urlLimpia, "&"], check=True)
-        #print("El script se ejecutó exitosamente.")
-        
-        #NO FUNCIONA EN PROCESOS HIJOS:
-        #proceso = subprocess.Popen(["sh", script_path, urlLimpia, "&"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, shell=True, start_new_session=True)
-        #proceso = subprocess.Popen(["sh", script_path, urlLimpia], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, start_new_session=False)
-        #proceso = subprocess.run(["sh", script_path, urlLimpia], check=True)
         proceso = subprocess.Popen(f"sh {script_path} {urlLimpia} &", shell=True)
         pid = proceso.pid
         print("PID del proceso:", pid)
-        # Imprimir la salida estándar y de error del proceso (opcional)
-        #salida_estandar, error_estandar = proceso.communicate()
-        #print("Salida estándar del script:", salida_estandar.decode())
-        #print("Error estándar del script:", error_estandar.decode())
     except subprocess.CalledProcessError as e:
         print(f"Error al ejecutar el script: {e}")
     except FileNotFoundError:
         print("No se encontró el archivo del script.")
 
-
-
-    #Espero a que termine el proceso
-    #proceso.wait()
-    #if proceso.returncode != 0:
-    #    print("Error durante la ejecución del script.")
-    #    print("Código de salida:", proceso.returncode)
-    # Imprimir un mensaje cuando el proceso ha terminado
-    #print("El proceso ha terminado")
 
 # Definir el manejador de solicitudes personalizado
 class RequestHandler(http.server.SimpleHTTPRequestHandler):
@@ -114,6 +81,21 @@ class RequestHandler(http.server.SimpleHTTPRequestHandler):
                 self.end_headers()
                 pathBuscado='Archivo no encontrado en: .' + template_path + "cliente.html"
                 self.wfile.write(pathBuscado.encode())
+        elif self.path == '/viewer':
+            try:
+                # Abre el archivo HTML solicitado en modo lectura binaria
+                with open('.' + template_path + self.path + ".html", 'rb') as file:
+                    content = file.read()
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                self.wfile.write(content)
+            except FileNotFoundError:
+                self.send_response(404)
+                self.send_header('Content-type', 'text/plain')
+                self.end_headers()
+                pathBuscado='Archivo no encontrado en: .' + template_path + "cliente.html"
+                self.wfile.write(pathBuscado.encode())
         # Servicio del contenidos de manera progresiva.
         elif self.path.startswith('/contenido/'):
             # Sirve archivos de video desde la carpeta "contenido"
@@ -142,7 +124,7 @@ class RequestHandler(http.server.SimpleHTTPRequestHandler):
                 self.end_headers()
                 response_data = json.dumps(archivos)
                 self.wfile.write(response_data.encode())
-        
+        # Anadir Cliente
         elif self.path == '/addCliente':
             # Obtiene la dirección IP del remitente
             client_ip = self.client_address[0]
@@ -158,9 +140,7 @@ class RequestHandler(http.server.SimpleHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
-            self.wfile.write(response_json.encode())
-    
-    
+            self.wfile.write(response_json.encode()) 
         else:
             self.send_response(404)
             self.send_header('Content-type', 'text/plain')
@@ -202,24 +182,117 @@ class RequestHandler(http.server.SimpleHTTPRequestHandler):
                 self.send_header('Content-type', 'text/plain')
                 self.end_headers()
                 self.wfile.write("Datos JSON no válidos".encode())
+        # Funcion para reproducir el video en los clientes
+        elif self.path == '/reproducirVideo':
+            content_length = int(self.headers['Content-Length'])
+            data = self.rfile.read(content_length)
+            data = json.loads(data.decode('utf-8'))
+            accion = data.get('accion')
+            video = data.get('video')
+
+            if accion == 'reproducir':
+                # Llama a la función para reproducir el video en los clientes registrados
+                #reproducir_video(video)
+                #Aqui vendria la accion de añadirlo a la cola de eventos
+                print("Llamo a reproducir video con video: " + video)
+                # Obtener la fecha y hora actual
+                fecha_actual = datetime.datetime.now()
+                # Formatear la fecha y hora como una cadena
+                fecha_actual_str = fecha_actual.strftime("%Y-%m-%d %H:%M:%S")
+                # Le añado el campo fecha al json
+                data["fecha"] = fecha_actual_str
+
+                # Agrego a la lista el mensaje
+                mensajes.append(data)
+
+                print("Contenido de la lista mensajes: " + str(mensajes))
+                self.send_response(200)
+                self.send_header('Content-type', 'application/json')
+                self.end_headers()
+                self.wfile.write(json.dumps({"mensaje": "Comando de reproducción enviado a los clientes"}).encode('utf-8'))
+                return
+            else:
+                self.send_response(200)
+                self.send_header('Content-type', 'application/json')
+                self.end_headers()
+                self.wfile.write(json.dumps({"mensaje": "Accion no reconocida"}).encode('utf-8'))
+                return
+
         else:
             self.send_response(404)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write("Endpoint no encontrado".encode())
 
-# Configurar el servidor para escuchar en el puerto 8000
-#with socketserver.TCPServer(("", 8000), RequestHandler) as httpd:
-#    print("Servidor en el puerto 8000")
-#    httpd.serve_forever()
 
-def iniciar_servidor():
+# Clase para manejar las conexiones de eventos
+class EventosHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/eventos':
+            # Obtén la dirección IP de la interfaz en la que se está escuchando
+            server_ip = socket.gethostbyname(socket.gethostname())
+
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/event-stream')
+            self.send_header('Cache-Control', 'no-cache')
+            self.send_header('Connection', 'keep-alive')
+            self.send_header('Access-Control-Allow-Origin', f'http://{server_ip}:8000')  # Reemplaza 8000 por el puerto adecuado
+            self.end_headers()
+            
+            # Agregar el cliente a la lista de clientes
+            clientes_eventos.append(self)
+
+            # Mantener la conexión abierta
+            try:
+                while True:
+                    # Envía eventos a todos los clientes de eventos
+                    if mensajes:
+                        evento = mensajes.pop(0)
+                        mensaje = f"data: {json.dumps(evento)}\n\n"
+                        
+                        # Envía el mensaje a todos los clientes de eventos
+                        for cliente in clientes_eventos:
+                            try:
+                                cliente.wfile.write(mensaje.encode('utf-8'))
+                            except Exception as e:
+                                print("Error al enviar mensaje al cliente:", str(e))
+                                # Elimina al cliente si hay un error
+                                clientes_eventos.remove(cliente)
+                    
+                    # Simula un intervalo de tiempo (ajusta según tus necesidades)
+                    time.sleep(1)
+            except Exception as e:
+                print("Cliente desconectado:", str(e))
+                # Elimina al cliente de la lista cuando se desconecta
+                clientes_eventos.remove(self)
+
+
+#def iniciar_servidor():
+#    PORT = 8000
+#    with http.server.ThreadingHTTPServer(("", PORT), RequestHandler) as httpd:
+#        print(f"Servidor en el puerto {PORT}")
+#        httpd.serve_forever()
+
+#if __name__ == "__main__":
+    # Iniciar el servidor en un hilo separado
+#    servidor_thread = threading.Thread(target=iniciar_servidor)
+#    servidor_thread.start()
+
+def iniciar_servidores():
     PORT = 8000
-    with http.server.ThreadingHTTPServer(("", PORT), RequestHandler) as httpd:
+    eventos_port = 8001  # Puerto para el servidor de eventos
+    # Crear el servidor web en un hilo
+    with ThreadingHTTPServer(("", PORT), RequestHandler) as httpd:
         print(f"Servidor en el puerto {PORT}")
+        
+        # Crear el servidor de eventos en otro hilo
+        eventos_server = ThreadingHTTPServer(("", eventos_port), EventosHandler)
+        eventos_thread = threading.Thread(target=eventos_server.serve_forever)
+        eventos_thread.start()
+        
+        # Iniciar el servidor web
         httpd.serve_forever()
 
 if __name__ == "__main__":
-    # Iniciar el servidor en un hilo separado
-    servidor_thread = threading.Thread(target=iniciar_servidor)
-    servidor_thread.start()
+    # Iniciar ambos servidores en hilos separados
+    iniciar_servidores()
